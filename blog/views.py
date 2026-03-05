2ed575557082a0b1f540242c03b6e24c0c9e1edc
SamipyaPhuyal <samipyaphuyal012@gmail.com>
2026-03-05 18:04:47 +0545

    added post routing

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView,DetailView
 # Create your views here.
 
 def home(request):
    return  render (request,"blog/home.html",context={"posts":Post.objects.all()})
+
+class PostListView(ListView):
+   model=Post
+   template_name="blog/home.html"
+   ordering=["-time"]
+   context_object_name="posts"
+
+class PostDetailView(DetailView):
+   model=Post
+   ordering=["-time"]
 
 def about(request):
    return render(request,"blog/about.html")
